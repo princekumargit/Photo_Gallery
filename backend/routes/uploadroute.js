69b8b77041f7d6da33c394eff4c534732ed8b9ad
Prince Kumar <prince1382002kumar@gmail.com>
2023-08-26 16:27:17 +0530

    Added delete feature

--- backend/routes/uploadroute.js
+++ backend/routes/uploadroute.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 const uploadMiddleware = require("../middlewares/multerMiddleware");
 const uploadModel = require("../models/uploadModel");
 const router = Router();
+const path = require("path");
+const fs = require("fs");
 
 router.get("/api/get", async (req, res) => {
   const allphotos = await uploadModel.find().sort({ createdAt: "descending" });
@@ -20,6 +22,24 @@ router.post("/api/save", uploadMiddleware.single("photo"), (req, res) => {
       res.send(data);
     })
     .catch((error) => console.console.log(error));
+});
+
+router.delete("/api/delete/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deletedPhoto = await uploadModel.findByIdAndDelete(id);
+    if (!deletedPhoto) {
+      return res.status(404).send("Photo not found");
+    }
+
+    const photoPath = path.join(__dirname,"..","public", "uploads", deletedPhoto.photo);
+    fs.unlinkSync(photoPath); // Delete the image file
+
+    res.send("Photo deleted successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 module.exports = router;
